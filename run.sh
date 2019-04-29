045e140218a6cdc874d70bf5cb0cce74c739e8b6
Sean <yeyuxiao@outlook.com>
2019-04-29 11:50:12 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,11 +9,11 @@ beam=10 # initial beam size for decoding
 maxword=5 # max word length for decoding
 
 # Create source file for the skip-gram model: word to index, and co-occurring word pairs
-python3 get_w2v.py $name $data $window
+python3 preprocess.py $name $data $window
 
 # Train a modified skip-gram model to derive CWS-optimized word embeddings,
 # and store cosine similarity scores for co-occurring word pairs.
-python3 preprocess.py $name $data $window
+python3 get_w2v.py $name $data $window
 
 # Decode test file and store the segmented text
 python3 decode.py $name $testdata $window $beam $maxword
